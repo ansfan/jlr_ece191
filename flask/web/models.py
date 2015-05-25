369507a181f34ddcca1987eeb4948d096d55c6e1
hijklmno <watson2272003@hotmail.com>
2015-05-25 00:50:38 -0700

    wording

--- flask/web/models.py
+++ flask/web/models.py
@@ -7,7 +7,7 @@ ROLE_ADMIN = 1
 
 class User(UserMixin):
 	# id = db.Column(db.Integer, primary_key = True)
-	# fb_id = db.Column(db.Integer, index = True, unique = True)
+	# unique_id = db.Column(db.Integer, index = True, unique = True)
 	# email = db.Column(db.String(120), index = True, unique = True)
 	# first_name = db.Column(db.String(64), index = True)
 	# last_name = db.Column(db.String(64), index = True)
@@ -15,7 +15,7 @@ class User(UserMixin):
 
 	def __init__(self, parse_id, auth_id, email, first_name, last_name, role_assigned):
 		self.id = parse_id
-		self.fb_id = auth_id
+		self.unique_id = auth_id
 		self.email = email
 		self.first_name = first_name
 		self.last_name = last_name
