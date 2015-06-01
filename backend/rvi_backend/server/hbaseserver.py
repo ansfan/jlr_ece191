ce5fc7a2e8f2aceb04e71248efb0c167e8e842a6
root <root@ip-172-31-17-174.us-west-2.compute.internal>
2015-06-01 06:37:57 +0000

    hbase is now happybase fml, lost data for days...

--- backend/rvi_backend/server/hbaseserver.py
+++ backend/rvi_backend/server/hbaseserver.py
@@ -10,6 +10,9 @@ For time being code is not modular... will update to create a table settings fil
 for future modurization.
 
 HBase Sink which takes messages from Kafka message queue and inserts into HBase
+
+NEW NOTE: WE WILL NOT BE DIRECTLY USING HBASE NOSQL... instead we will be using 
+Apache Phoenix which is a SQL wrapper for HBASE.
 """
 
 import os, threading, base64
@@ -20,9 +23,11 @@ from rvijsonrpc import RVIJSONRPCServer
 import __init__
 from __init__ import __RVI_LOGGER__ as logger
 
+#import jaydebeapi, jpype
 import json
 from kafka import KafkaClient, SimpleConsumer
 from starbase import Connection
+import happybase
 
 class HBaseServer(threading.Thread):
     """
@@ -33,38 +38,39 @@ class HBaseServer(threading.Thread):
         threading.Thread.__init__(self)
         self.kafka = KafkaClient(kafka_url)
         self.topic = kafka_topic
-        self.hbase_connect = Connection(host=hbase_url, port=hbase_rest_port)
+        self.hbase_connect = happybase.Connection(hbase_url)
         self.server_on_flag = True        
         self.cons = SimpleConsumer(self.kafka, None, self.topic)
         self.cons.seek(0,2)
         self.m = None
-        self.car_table = self.hbase_connect.table('rvi_taxi')
+        self.car_table = self.hbase_connect.table('rvi')
         self.payload = None
         self.vin = None
         self.time = None
         self.data = None
         self.row_key = None
+        self.count = 0
+        self.sql_insert = None 
 
     def run(self):
-        
         while self.server_on_flag:
 
             self.m = self.cons.get_message(block=False)
            
             if (self.m is not None):
                 self.payload = json.loads(self.m.message.value)
-                self.vin = self.payload['vin']
-                self.time = self.payload['timestamp']
-                self.data = self.payload['data']
-
-                self.row_key = self.vin+self.time
+                self.vin = str(self.payload['vin'])
+                self.time = str(self.payload['timestamp'])
+                self.data = str(self.payload['data'])
                 
-                if self.car_table.insert(self.row_key,{'car':{'data':self.data}}) == 200:
-                    logger.info('HBase Server: key: %s, table: %s, car{data: %s}.', self.time, 'rvi_taxi', self.data)
-
-                else:
-                    logger.info('Data Push into HBase unsuccessful...')
-
+                self.row_key = self.vin+self.time
+                try:
+                    self.car_table.put(self.row_key,{'car:data':self.data})
+                    self.count = self.count + 1
+                    logger.info('HBase Server: key: %s, table: %s, car{data: %s}. Message number: %s', self.row_key, 'rvi', self.data, str(self.count))     
+           
+                except Exception as e:
+                    logger.info('%s,Data Push into HBase unsuccessful...', e)
 
             else:
                 sleep(1/5)
