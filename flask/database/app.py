75291acb9e6a95f33f2e299d083d03c958d482c6
hijklmno <watson2272003@hotmail.com>
2015-06-11 09:42:13 -0700

    removing unused modules

--- flask/database/app.py
+++ flask/database/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, url_for, jsonify, request
 import time
 import requests
-from celery import Celery
 import json
 import settings_app as settings
 
@@ -17,12 +16,6 @@ def jsonifyCSData(data):
 		'time' : str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(data_splitted[3]))))
 	}
 	return result
-	
-# Import data
-# testdata = []
-# with open('../cabspottingdata/new_abboip2.txt') as inputfile:
-# 	for line in inputfile:
-# 		testdata.append(jsonifyCSData(line))
 
 ########################
 # Flask Initialization #
@@ -47,22 +40,6 @@ hbasetable = hbasepull.RVIHBaseTable()
 def maxDateWrapper(vin, output):
 	output.put([vin, hbasetable.max_date(vin)])
 
-####################
-# Celery Functions #
-####################
-app.config['CELERY_BROKER_URL'] = settings.CELERY_BROKER_URL
-app.config['CELERY_RESULT_BACKEND'] = settings.CELERY_RESULT_BACKEND
-
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
-@celery.task(bind=True)
-def send_cab_data(self, dest, payload):
-	with app.app_context():
-		headers = {'Content-Type': 'application/json'}
-		r = requests.post(dest, data=json.dumps(payload), headers=headers)
-	return True
-
 #################
 # Flask Routing #
 #################
@@ -79,8 +56,6 @@ def webhook():
 		data = request.get_json()
 
 		# implement error catcher http://flask.pocoo.org/docs/0.10/patterns/apierrors/
-		# print data['source']
-		# print data['car_name']
 		response_address = data['source']
 		response_attr = data['car_name']
 
@@ -113,16 +88,6 @@ def webhook():
 					vin1.start()
 
 					rvi_thread_pool[response[0]] = vin1
-
-		# for items in testdata:
-		# 	payload = {
-		# 		'data' : items,
-		# 		'car_name' : response_attr
-		# 	}
-		# 	task = send_cab_data.apply_async(args=[response_address, payload])
-
-		# if task:
-		# 	print "Sent complete."
 
 	return "OK\n"
 
--- flask/database/settings_app.py
+++ flask/database/settings_app.py
@@ -1,14 +1,11 @@
 # Flask
 SECRET_KEY = 'secret!'
 
-# Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
 # RVI
 DATABASE_WEBHOOK_URL = 'http://52.25.32.18:8123/webhook/'
 FLASK_WEBHOOK_URL = 'http://52.24.215.226/webhook/'
 RVI_KAFKA_ENDPOINT = 'master:6667'
 
+# HBase
 HBASE_IP = '172.31.17.174'
 HBASE_TABLE = 'rvi'
